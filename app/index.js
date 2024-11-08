b466eb7aa17a3b64acf9a06fc59de225d3d96235
kavin <tkavin28@gmail.com>
2024-11-08 13:07:47 +0530

    updated

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from service1!'));
+app.get('/', (req, res) => res.send('Hello and hii from service1!'));
 
 app.listen(PORT, () => console.log(`Service running on port ${PORT}`));
